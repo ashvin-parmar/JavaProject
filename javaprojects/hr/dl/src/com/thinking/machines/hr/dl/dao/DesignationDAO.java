d30d8deb4ff041723ae9b0d7d9bdea2557a168fd
Ashvin <ashwinparmar8181@gmail.com>
2025-01-06 17:48:42 +0530

    JavaProject: HR: DesignationDAO interface created complete and implementation pending

---
+++ javaprojects/hr/dl/src/com/thinking/machines/hr/dl/dao/DesignationDAO.java
@@ -0,0 +1,45 @@
+package com.thinking.machines.hr.dl.dao;
+import java.io.*;
+import java.util.*;
+import com.thinking.machines.hr.dl.exceptions.*;
+import com.thinking.machines.hr.dl.interfaces.dto.*;
+import com.thinking.machines.hr.dl.interfaces.dao.*;
+public class DesignationDAO
+{
+public void add(DesignationDTOInterface designationDTO) throws DAOException	//Here 'throws' for checked exception
+{
+throw new DAOException("Not yer implemented");	//here 'throw' for throwing exception and this line for not implementing anything at initier level
+}
+public void update(DesignationDTOInterface designationDTO) throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+public void delete(int code) throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+public TreeSet<DesignationDTOInterface> getAll() throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+public DesignationDTOInterface getByCode(int code) throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+public DesignationDTOInterface getByTitle(String title) throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+public boolean codeExist(int coed) throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+public boolean titleExist(String title) throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+public int getCount() throws DAOException
+{
+throw new DAOException("Not yer implemented");
+}
+}
--- javaprojects/hr/dl/src/compile.sh
+++ javaprojects/hr/dl/src/compile.sh
@@ -1,3 +1,5 @@
 javac -d ../classes/ com/thinking/machines/hr/dl/exceptions/*.java
 javac -d ../classes/ com/thinking/machines/hr/dl/interfaces/dto/*.java
 javac -d ../classes/ com/thinking/machines/hr/dl/dto/*.java
+javac -d ../classes/ com/thinking/machines/hr/dl/interfaces/dao/*.java
+javac -d ../classes/ com/thinking/machines/hr/dl/dao/*.java
