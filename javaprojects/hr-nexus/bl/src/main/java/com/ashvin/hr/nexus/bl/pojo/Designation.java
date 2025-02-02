05dac936ab90428d6d6883b9c652bf72e8720c29
Ashvin <ashwinparmar8181@gmail.com>
2025-02-02 22:08:28 +0530

    HR-Nexus: DesignationInterface and Designation Pojo Class Implementation Complete

---
+++ javaprojects/hr-nexus/bl/src/main/java/com/ashvin/hr/nexus/bl/pojo/Designation.java
@@ -0,0 +1,40 @@
+package com.ashvin.hr.nexus.bl.pojo;
+
+import com.ashvin.hr.nexus.bl.interfaces.pojo.*;
+
+public class Designation implements DesignationInterface
+{
+private int code;
+private String title;
+public void setCode(int code)
+{
+this.code=code;
+}
+public int getCode()
+{
+return this.code;
+}
+public void setTitle(String title)
+{
+this.title=title;
+}
+public String getTitle()
+{
+return this.title;
+}
+public int hashCode()
+{
+return this.code;
+}
+public boolean equals(Object object)
+{
+if(!(object instanceof DesignationInterface)) return false;
+DesignationInterface designation;
+designation=(DesignationInterface)object;
+return this.code==designation.getCode();
+}
+public int compareTo(DesignationInterface designation)
+{
+return this.code-designation.getCode();
+}
+}
