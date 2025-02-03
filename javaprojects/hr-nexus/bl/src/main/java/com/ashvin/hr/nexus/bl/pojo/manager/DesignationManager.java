85d43035f958fdc6de086a892335eeb03c5231ab
Ashvin <ashwinparmar8181@gmail.com>
2025-02-04 03:47:04 +0530

    HR-Nexus: BL: DesignationManager => Singletone Pattern Method Implementation Done

--- javaprojects/hr-nexus/bl/src/main/java/com/ashvin/hr/nexus/bl/pojo/manager/DesignationManager.java
+++ javaprojects/hr-nexus/bl/src/main/java/com/ashvin/hr/nexus/bl/pojo/manager/DesignationManager.java
@@ -12,7 +12,44 @@ import com.ashvin.hr.nexus.dl.dao.*;
 import java.util.*;
 public class DesignationManager implements DesignationManagerInterface
 {
-
+private Map<Integer,DesignationInterface> codeWiseDesignationMap;
+private Map<String,DesignationInterface> titleWiseDesignationMap;
+private Set<DesignationInterface> designationSet;
+private static DesignationManagerInterface designationManager=null;
+private DesignationManager() throws BLException
+{
+populateDataStructures();
+}
+private void populateDataStructures() throws BLException
+{
+this.codeWiseDesignationMap=new HashMap<>();
+this.titleWiseDesignationMap=new HashMap<>();
+this.designationSet=new TreeSet<>();
+try
+{
+DesignationInterface designation;
+Set<DesignationDTOInterface> dlDesignations=(new DesignationDAO()).getAll();
+for(DesignationDTOInterface dlDesignation:dlDesignations)
+{
+designation=new Designation();
+designation.setCode(dlDesignation.getCode());
+designation.setTitle(dlDesignation.getTitle());
+this.codeWiseDesignationMap.put(new Integer(designation.getCode()),designation);
+this.titleWiseDesignationMap.put(designation.getTitle().toUpperCase(),designation);
+this.designationSet.add(designation);
+}
+}catch(DAOException daoException)
+{
+BLException blException=new BLException();
+blException.setGenericException(daoException.getMessage());
+throw blException;
+}
+}
+public DesignationManagerInterface getDesignationManager() throws BLException
+{
+if(this.designationManager==null) this.designationManager=new DesignationManager();
+return this.designationManager;
+}
 public void add(DesignationInterface designation) throws BLException
 {
 BLException blException=new BLException();
