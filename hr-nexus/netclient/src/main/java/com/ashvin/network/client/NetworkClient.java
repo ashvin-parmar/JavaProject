93154db44e4238bfc9e2234f1617261df2f9a494
Ashvin <ashwinparmar8181@gmail.com>
2025-06-28 01:22:01 +0530

    HR-Nexus: Networking: netclient-> NetworkClient class send method [Pending]

---
+++ hr-nexus/netclient/src/main/java/com/ashvin/network/client/NetworkClient.java
@@ -0,0 +1,19 @@
+package com.ashvin.network.client;
+import com.ashvin.network.common.*;
+import com.ashvin.network.common.exceptions.*;
+
+public class NetworkClient 
+{
+public Response send(Request request) throws NetworkException
+{
+/*
+Wrap all the network/socket programming code over here.
+1. Serialize Request Object.
+2. Connect to the server.
+3. Send header and then the serialized form in chunks.
+4. Receive back header and then the serialized form of response. 
+5. return the reference of Response object.
+*/
+return null;
+}
+}
