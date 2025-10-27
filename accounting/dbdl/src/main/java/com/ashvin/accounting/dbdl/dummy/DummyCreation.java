f93ffbb4f989e943113f7a108467e5b3c9631f9e
Ashvin <ashwinparmar8181@gmail.com>
2025-10-28 04:02:24 +0530

    Accounting: Purchase Dummy records function created and tested | Records added in database |

--- accounting/dbdl/src/main/java/com/ashvin/accounting/dbdl/dummy/DummyCreation.java
+++ accounting/dbdl/src/main/java/com/ashvin/accounting/dbdl/dummy/DummyCreation.java
@@ -8,7 +8,7 @@ import java.time.*;
 public class DummyCreation
 {
 private String alphabetsStr="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-private String numbericStr="1234567890";
+private String numericStr="1234567890";
 private String specialStr="!@#$%^&*_+-";
 private Random random;
 public DummyCreation()
@@ -114,7 +114,6 @@ public void createDummySales3000(java.util.Date startDate)
 {
 Date tmpDate=null;
 Duration duration=Duration.ofMillis(0);
-System.out.println(startDate.getTime());
 duration=duration.plusMillis(startDate.getTime());
 long durationRange=Duration.ofMinutes(20).toMillis();
 long randomInteger;
@@ -125,16 +124,14 @@ duration=duration.minusDays(1);
 for(int i=0;i<30;i++)
 {
 duration=duration.plusDays(1);
-System.out.println(duration.toMillis());
-//tmpDate=new Date(startDate.getTime()+(duration.toMillis()));
-System.out.println("---------------");
+//System.out.println("---------------Day ends --------");
 countRecords=0;
 while(countRecords<100)
 {
 randomInteger=random.nextLong(durationRange)+Duration.ofMinutes(2).toMillis();
 duration=duration.plusMillis(randomInteger);
 tmpDate=new Date(duration.toMillis());
-System.out.println("RandomNumber: "+randomInteger+", Millies: "+duration.toMillis()+", Date: "+tmpDate);
+//System.out.println("RandomNumber: "+randomInteger+", Millies: "+duration.toMillis()+", Date: "+tmpDate);
 try
 {
 sale=new Sale();
@@ -149,6 +146,40 @@ countRecords++;
 {
 System.out.println(daoException.getMessage());
 }
+}
+}
+}
+public void createDummyPurchases4000()
+{
+long randomInteger;
+PurchaseDAO purchaseDAO=new PurchaseDAO();
+StringBuilder sb=null;
+String billNumber="";
+String fromStr=alphabetsStr+numericStr;
+int range=fromStr.length();
+Purchase purchase=null;
+int countRecords=0;
+while(countRecords<4000)
+{
+try
+{
+sb=new StringBuilder();
+for(int i=0;i<15;i++)
+{
+sb.append(fromStr.charAt(random.nextInt(range)));
+}
+billNumber=sb.toString();
+purchase=new Purchase();
+purchase.setBillNumber(billNumber);
+purchase.setSupplierCode(random.nextInt(100)+1);
+purchase.setItemCode(random.nextInt(400)+1);
+purchase.setQuantity(random.nextInt(10)+1);
+purchase.setRate(random.nextInt(10)+10);
+purchaseDAO.addPurchase(purchase);
+countRecords++;
+}catch(DAOException daoException)
+{
+System.out.println(daoException.getMessage());
 }
 }
 }
