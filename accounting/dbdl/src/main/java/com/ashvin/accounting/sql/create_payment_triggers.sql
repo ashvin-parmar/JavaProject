e1447f133fe54d9206580db897d903b65a1e8f18
Ashvin <ashwinparmar8181@gmail.com>
2025-11-05 16:08:05 +0530

    Accounting: All 3 triggers for Insert | Update | Delete created for all All 4 accounting tables Sale, Receipt, Purchase, Payment | Total 12 triggers added |

--- accounting/dbdl/src/main/java/com/ashvin/accounting/sql/create_payment_triggers.sql
+++ accounting/dbdl/src/main/java/com/ashvin/accounting/sql/create_payment_triggers.sql
@@ -6,4 +6,18 @@ begin
 	update supplier set total_payment=total_payment+(NEW.amount) where NEW.supplier_code=supplier.code;
 end $$
 
+create trigger update_payment_trigger
+before update on payment
+for each row
+begin 
+	update supplier set total_payment=total_payment-(OLD.amount)+(NEW.amount) where NEW.supplier_code=supplier.code;
+end $$
+
+create trigger delete_payment_trigger
+before delete on payment
+for each row
+begin 
+	update supplier set total_payment=total_payment-(OLD.amount) where OLD.supplier_code=supplier.code;
+end $$
+
 delimiter ;
