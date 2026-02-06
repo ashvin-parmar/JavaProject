4705888588664abb128c2beafbf6f19fae7479ab
Ashvin <ashwinparmar8181@gmail.com>
2026-02-06 19:18:59 +0530

    N2P: Two PipeLine Send and Receive [byte stream data transaction] Complete the implementation part

--- networking/n2p/PipeLine2Receive.java
+++ networking/n2p/PipeLine2Receive.java
@@ -47,19 +47,93 @@ public void run()
 {
 try
 {
-byte bytes[];
-int x;
+OutputStream os=this.outputStream;
+InputStream is=this.inputStream;
+int bytesReadCount;
+long bytesToRecieve;
+long bytesToSend;
+long requestLength;
+long responseLength;
+byte requestBytes[];
+byte responseBytes[];
+byte ack[]=new byte[1];
+byte header[]=new byte[1024];
+byte tmp[]=new byte[1024];
+int i,k,l;
+long j;
+bytesToRecieve=1024;
+j=0;
+k=0;
+while(j<bytesToRecieve)
+{
+bytesReadCount=is.read(tmp);
+if(bytesReadCount==-1) continue;
+for(i=0;i<bytesReadCount;i++)
+{
+header[k]=tmp[i];
+k++;
+}
+j+=bytesReadCount;
+}
+requestLength=0;
+i=1023;
+j=1;
+while(i>=0)
+{
+requestLength=requestLength+(header[i]*j);
+j*=10;
+i--;
+}
+ack[0]=(byte)1;
+os.write(ack,0,1);
+os.flush();
+requestBytes=new byte[(int)requestLength];
+j=0;
+k=0;
+while(j<requestLength)
+{
+bytesReadCount=is.read(tmp);
+if(bytesReadCount==-1) continue;
+for(i=0;i<bytesReadCount;i++)
+{
+requestBytes[k]=tmp[i];
+k++;
+}
+j+=bytesReadCount;
+}
+responseBytes=application.onRequestBytes(clientId,requestBytes);
+responseLength=responseBytes.length;
+i=1023;
+j=responseLength;
+header=new byte[1024];
+while(i>=0)
+{
+header[i]=(byte)(j%10);
+j/=10;
+i--;
+}
+os.write(header,0,1024);
+os.flush();
 while(true)
 {
-x=inputStreamReader.read();
-if(x==-1) continue;
-
-// Code to extract data from request and create 
-// header+content into byte[]
-// then call the server method through variable application
-// deliver the byte[].
-//byte[] responseBytes=application.onRequestBytes(clientId,bytes);
-// send back response bytes
+bytesReadCount=is.read(ack);
+if(bytesReadCount==-1) continue;
+break;
+}
+int chunkSize=1024;
+j=0;
+while(j<responseLength)
+{
+if((responseLength-j)<chunkSize) chunkSize=(int)(responseLength-j);
+os.write(responseBytes,(int)j,chunkSize);
+os.flush();
+j+=chunkSize;
+}
+while(true)
+{
+bytesReadCount=is.read(ack);
+if(bytesReadCount==-1) continue;
+break;
 }
 }catch(Exception exception)
 {
